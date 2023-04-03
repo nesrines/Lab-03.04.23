b2bfbf8e8d7dcb70709c2ebcbd608039b6bf5e36
Nasrin F. Asadli <nasrinfa@code.local>
2023-04-03 17:04:00 +0400

    added 3 new algoritms

---
+++ Max-min/Max-min/Program.cs
@@ -0,0 +1,30 @@
+﻿Console.WriteLine("How many numbers do you want to add?");
+int n = Convert.ToInt32(Console.ReadLine());
+
+if (n < 2)
+        { Console.WriteLine("Please enter at least '2'."); }
+
+else
+{
+    int[] Numbers = new int [n];
+    Console.WriteLine("Add " + n + " numbers:");
+
+    for (int i = 0; i < n; i++)
+    {
+        Numbers[i] = Convert.ToInt32(Console.ReadLine());
+    }
+
+    int max = Numbers[0];
+    int min = Numbers[0];
+
+    for (int i = 0; i < Numbers.Length; i++)
+    {
+        if (max < Numbers[i])
+        { max = Numbers[i]; }
+
+        else if (min > Numbers[i])
+        { min = Numbers[i]; }
+    }
+
+    Console.WriteLine("max = " + max + "; min = " + min);
+}
---
+++ Sorting/Sorting/Program.cs
@@ -0,0 +1,32 @@
+﻿Console.WriteLine("How many numbers do you want to add?");
+int n = Convert.ToInt32(Console.ReadLine());
+
+if (n < 5)
+{ Console.WriteLine("Please enter at least '5'."); }
+
+else
+{
+    int[] Numbers = new int[n];
+    Console.WriteLine("Add " + n + " numbers:");
+
+    for (int i = 0; i < n; i++)
+    {
+        Numbers[i] = Convert.ToInt32(Console.ReadLine());
+    }
+
+    for (int i = 0; i < Numbers.Length; i++)
+    {
+        for (int j = 0; j < Numbers.Length; j++)
+        {
+            if (i != j && Numbers[j] > Numbers[i])
+            {
+                int r = Numbers[j];
+                Numbers[j] = Numbers[i];
+                Numbers[i] = r;
+            }
+        }
+    }
+
+    for (int i = 0; i < Numbers.Length; i++)
+    { Console.Write(Numbers[i] + ", "); }
+}
